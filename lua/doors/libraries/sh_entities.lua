2e072509a127877c47f805349c69d9786609b47b
Amy Jeanes <amyjeanes23@gmail.com>
2025-12-14 21:42:18 +0000

    add hooks for interior/exterior added/removed

--- lua/doors/libraries/sh_entities.lua
+++ lua/doors/libraries/sh_entities.lua
@@ -3,9 +3,13 @@
 Doors.Interiors={}
 function Doors:AddInterior(e)
     self.Interiors[e]=true
+
+    hook.Call("Doors-InteriorAdded", GAMEMODE, e)
 end
 function Doors:RemoveInterior(e)
     self.Interiors[e]=nil
+
+    hook.Call("Doors-InteriorRemoved", GAMEMODE, e)
 end
 function Doors:GetInteriors()
     return self.Interiors
@@ -14,9 +18,13 @@ end
 Doors.Exteriors={}
 function Doors:AddExterior(e)
     self.Exteriors[e]=true
+
+    hook.Call("Doors-ExteriorAdded", GAMEMODE, e)
 end
 function Doors:RemoveExterior(e)
     self.Exteriors[e]=nil
+
+    hook.Call("Doors-ExteriorRemoved", GAMEMODE, e)
 end
 function Doors:GetExteriors()
     return self.Exteriors
